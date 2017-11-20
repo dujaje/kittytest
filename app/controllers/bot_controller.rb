7e8699351a6da00e47a4a1d7d4fd406fae07c2c4
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-20 23:20:07 +0000

    added webhook method

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,2 +1,9 @@
 class BotController < ApplicationController
+  def webhook
+   if params[‘hub.verify_token’] == “mytoken”
+     render text: params[‘hub.challenge’] and return
+   else
+     render text: ‘error’ and return
+   end
+  end
 end
