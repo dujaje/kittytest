718fd381cf37414c4f6b55bd9ad58feb78d26348
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-20 23:44:22 +0000

    lalala

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,9 +1,11 @@
-class BotController < ApplicationController
-  def webhook
+class WebhooksController < ApplicationController
+  def messenger
    if params[‘hub.verify_token’] == “mytoken”
+     puts "success"
      render text: params[‘hub.challenge’] and return
    else
      render text: ‘error’ and return
+     puts "error"
    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  post 'bot', to: 'bot#webhook'
+  get 'webhooks', to: 'webhooks#messenger'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
