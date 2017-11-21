b33902ab6e4f11a17d50b3baef30f5eca31087cf
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-21 16:09:07 +0000

    added woo

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -37,11 +37,11 @@ class WebhooksController < ApplicationController
                                           "image_url": "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/23658371_154645468621882_4383150760338193341_n.png?oh=f21774eb176fef440d61a6a20c577ba7&oe=5A9BF90F",
                                           "buttons": [{
                                             "type": "web_url",
-                                            "url": "http://kittymoneysplitter.herokuapp.com/extension/welcome",
+                                            "url": "https://2bbc8ecf.ngrok.io/extension/welcome",
                                             "title": "Open Kitty"
                                           }, {
                                             "type": "postback",
-                                            "title": "#{ENV['DEVELOPER_TOKEN']} Meow",
+                                            "title": "Meow",
                                             "payload": "Payload for first element in a generic bubble",
                                           }],
                                         }]
