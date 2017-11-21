08a198d63721eb828c2339ef2ac5fb8d88659636
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-21 12:45:06 +0000

    created namespace routing for messenger extension

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get 'webhooks', to: 'webhooks#messenger'
   post 'webhooks', to: 'webhooks#receive_message'
 
+  namespace :extension do
+     get 'welcome', to: 'pages#home'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
