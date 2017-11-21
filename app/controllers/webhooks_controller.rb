2b75691052c93df1af6552ebf38cc2ebdfad69cb
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-21 00:27:09 +0000

    lalalala|

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,5 @@
 class WebhooksController < ApplicationController
+  protect_from_forgery with: :null_session
   def messenger
     verification_token = "mytoken"
     if params["hub.verify_token"] == verification_token
@@ -16,7 +17,23 @@ class WebhooksController < ApplicationController
       entry["messaging"].each do |messaging|
         sender = messaging["sender"]["id"]
         text = messaging["message"]["text"]
+        my_reply = {
+                      "messaging_type": "RESPONSE",
+                      "recipient": {
+                        "id": "#{sender}"
+                      },
+                      "message": {
+                        "text": "meow #{text}"
+                      }
+                    }
+        puts HTTP.post(url, json: my_reply)
       end
     end
+    render "recieved_data"
   end
+
+  def url
+    "https://graph.facebook.com/v2.6/me/messages?access_token=EAAdHTu6J8uMBAAc19ijgenHlcZABGERZCLGUX6X3QzYe0ZA2C2wac0XcDv2IRXL525sLBsZARZAksKNZCQfc2ayZCib6ZAd7PTfnG5O4VJ7Xd4NRZAWX0HEhZBrTZCEHrZCzNCkdSD7mMuTT0iYdB7jrAdFOKzgW37YZCP7ZAUGV0IeJtZBvQZDZD"
+  end
+
 end
