8a3f5e4bc72f922072f279fba1f82dc88d3e0863
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-22 16:17:16 +0000

    blah

--- app/views/extension/pages/facebook_curls.sh
+++ app/views/extension/pages/facebook_curls.sh
@@ -1,19 +1,32 @@
 # Facebook curls
 
+# This whitelists a domain
 curl -X POST -H "Content-Type: application/json" -d '{
   "whitelisted_domains":[
     "https://f5aa87a1.ngrok.io/extension/*"
   ]
 }' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAdHTu6J8uMBAAc19ijgenHlcZABGERZCLGUX6X3QzYe0ZA2C2wac0XcDv2IRXL525sLBsZARZAksKNZCQfc2ayZCib6ZAd7PTfnG5O4VJ7Xd4NRZAWX0HEhZBrTZCEHrZCzNCkdSD7mMuTT0iYdB7jrAdFOKzgW37YZCP7ZAUGV0IeJtZBvQZDZD"
 
+# ADAPT this to whitelist domain. "add" != "remove"
+curl -X POST -H "Content-Type: application/json" -d '{
+  "setting_type" : "domain_whitelisting",
+  "whitelisted_domains" : ["https://9569869d.ngrok.io"],
+  "domain_action_type": "add"
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAdHTu6J8uMBAAc19ijgenHlcZABGERZCLGUX6X3QzYe0ZA2C2wac0XcDv2IRXL525sLBsZARZAksKNZCQfc2ayZCib6ZAd7PTfnG5O4VJ7Xd4NRZAWX0HEhZBrTZCEHrZCzNCkdSD7mMuTT0iYdB7jrAdFOKzgW37YZCP7ZAUGV0IeJtZBvQZDZD"
+
+
+# This initiates the home url
 curl -X POST -H "Content-Type: application/json" -d ' {
   "home_url" : {
-     "url": "https://f5aa87a1.ngrok.io/extension/welcome",
+     "url": "http://9569869d.ngrok.io",
      "webview_height_ratio": "tall",
      "webview_share_button": "hide",
      "in_test":true
   }
 }' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAdHTu6J8uMBAAc19ijgenHlcZABGERZCLGUX6X3QzYe0ZA2C2wac0XcDv2IRXL525sLBsZARZAksKNZCQfc2ayZCib6ZAd7PTfnG5O4VJ7Xd4NRZAWX0HEhZBrTZCEHrZCzNCkdSD7mMuTT0iYdB7jrAdFOKzgW37YZCP7ZAUGV0IeJtZBvQZDZD"
+
+
+
 
 {
       "attachment": {
