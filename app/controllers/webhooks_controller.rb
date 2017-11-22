c36288fbc197439435056dae003bed96540d570a
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-22 11:03:20 +0000

    ghjkl

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -37,11 +37,13 @@ class WebhooksController < ApplicationController
                                           "image_url": "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/23658371_154645468621882_4383150760338193341_n.png?oh=f21774eb176fef440d61a6a20c577ba7&oe=5A9BF90F",
                                           "buttons": [{
                                             "type": "web_url",
-                                            "url": "https://kittymoneysplitter.herokuapp.com/extension/welcome",
-                                            "title": "Open Kitty"
+                                            "url": "https://2bbc8ecf.ngrok.io/extension/welcome",
+                                            "title": "Open Kitty",
+                                            "webview_height_ratio": "full",
+                                            "messenger_extensions": true
                                           }, {
                                             "type": "postback",
-                                            "title": "Meow",
+                                            "title": "Jamie: Meow",
                                             "payload": "Payload for first element in a generic bubble",
                                           }],
                                         }]
