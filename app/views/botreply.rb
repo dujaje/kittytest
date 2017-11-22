8323b31514afd5a937399b08a89a5c1d80409e7e
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-22 12:13:06 +0000

    reply page

---
+++ app/views/botreply.rb
@@ -0,0 +1,31 @@
+def reply(sender)
+  my_reply = {
+                "messaging_type": "RESPONSE",
+                "recipient": {
+                  "id": "#{sender}"
+                },
+                "message": {
+                "attachment": {
+                                "type": "template",
+                                "payload": {
+                                "template_type": "generic",
+                                  "elements": [{
+                                  "title": "Welcome to Kitty",
+                                    "subtitle": "The best app for money sharing",
+                                    "image_url": "https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/23658371_154645468621882_4383150760338193341_n.png?oh=f21774eb176fef440d61a6a20c577ba7&oe=5A9BF90F",
+                                    "buttons": [{
+                                      "type": "web_url",
+                                      "url": "https://kittymoneysplitter.herokuapp.com/extension/welcome",
+                                      "title": "Open Kitty"
+                                    }, {
+                                      "type": "postback",
+                                      "title": "Guy: Meow",
+                                      "payload": "Payload for first element in a generic bubble",
+                                    }],
+                                  }]
+                                }
+                              }
+                }
+              }
+  return my_reply
+end
