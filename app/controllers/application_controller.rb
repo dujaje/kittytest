0433b4f67215060997b69308951e0969f4c3c99e
leonard percival <leonardpercival@gmail.com>
2017-11-23 16:42:43 +0000

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
+    return nil unless params[:user_id].present?
     User.find(params[:user_id])
   end
 
   def current_group
+    return nil unless params[:user_id].present?
     Group.find(params[:group_id])
   end
 end
