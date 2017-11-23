7365afc86821222483e6caabe7b7cc99c33f9e6b
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-23 14:24:37 +0000

    fb curls sorted

--- facebook_curls.sh
+++ facebook_curls.sh
@@ -1,5 +1,8 @@
 # Facebook curls
 
+# this gets all info from fb
+curl -X GET "https://graph.facebook.com/v2.6/me/messenger_profile?fields=whitelisted_domains,payment_settings,target_audience,home_url,account_linking_url,greeting,persistent_menu,get_started&access_token=EAAdHTu6J8uMBAAc19ijgenHlcZABGERZCLGUX6X3QzYe0ZA2C2wac0XcDv2IRXL525sLBsZARZAksKNZCQfc2ayZCib6ZAd7PTfnG5O4VJ7Xd4NRZAWX0HEhZBrTZCEHrZCzNCkdSD7mMuTT0iYdB7jrAdFOKzgW37YZCP7ZAUGV0IeJtZBvQZDZD"
+
 # This whitelists a domain
 curl -X POST -H "Content-Type: application/json" -d '{
   "whitelisted_domains":[
