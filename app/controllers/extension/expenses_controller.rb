5be36827c2d932679e0044e6738b4fc542822a9d
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-27 17:08:16 +0000

    splitting working

--- app/controllers/extension/expenses_controller.rb
+++ app/controllers/extension/expenses_controller.rb
@@ -11,12 +11,14 @@ class Extension::ExpensesController < ApplicationController
     @title = getparams[:title]
     @amount_cents = getparams[:amount_cents]
     @description = getparams[:description]
+    @involved_group_string = getparams[:involved_group]
     @expense = Expense.create!(
       title: @title,
       description: @description,
       amount_cents: @amount_cents,
       user_id: @user_id,
       group_id: @group_id)
+    equal_splitter(@expense, @involved_group_string)
     redirect_to extension_group_path(@group_id, user_id: @user_id, group_id: @group_id)
   end
 
@@ -25,20 +27,21 @@ class Extension::ExpensesController < ApplicationController
 
   private
 
-  def equal_splitter(expense, involved_group)
+  def equal_splitter(expense, involved_group_string)
+    involved_group = involved_group_string.split(",")
     number_involved = involved_group.length
     an_equal_split = expense.amount_cents.to_i / number_involved
-    involved_group.each do |member|
-      if member == creator
+    involved_group.each do |member_id|
+      if member_id.to_i == expense.user.id
         next
       end
-      split = Split.new(expense_id: expense, user_id: member, amount_cents: an_equal_split)
-      split.save
+      split = Split.new(expense_id: expense.id, user_id: member_id, amount_cents: an_equal_split)
+      split.save!
     end
   end
 
   def getparams
-    params.require(:expense).permit(:title, :amount_cents, :description, :user_id, :group_id)
+    params.require(:expense).permit(:title, :amount_cents, :description, :user_id, :group_id, :involved_group)
   end
 
 end
