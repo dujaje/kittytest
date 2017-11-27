4fe0351b69164588dfee9884a55f495a546218ff
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-27 14:18:01 +0000

    equal splitter method created

--- app/controllers/extension/expenses_controller.rb
+++ app/controllers/extension/expenses_controller.rb
@@ -10,4 +10,18 @@ class Extension::ExpensesController < ApplicationController
 
   def show
   end
+
+  private
+
+  def equal_splitter(expense, involved_group)
+    number_involved = involved_group.length
+    an_equal_split = expense.amount_cents.to_i / number_involved
+    involved_group.each do |member|
+      if member == creator
+        next
+      end
+      split = Split.new(expense_id: expense, user_id: member, amount_cents: an_equal_split)
+      split.save
+    end
+  end
 end
