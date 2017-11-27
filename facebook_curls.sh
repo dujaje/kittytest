3776e1b042d5789b7dd2e70abc9e705a6f234c00
leonard percival <leonardpercival@gmail.com>
2017-11-27 15:53:18 +0000

    welcome finished

--- facebook_curls.sh
+++ facebook_curls.sh
@@ -16,33 +16,33 @@ curl -X POST -H "Content-Type: application/json" -d '{
 # ADAPT this to whitelist domain. "add" != "remove"
 curl -X POST -H "Content-Type: application/json" -d '{
   "setting_type" : "domain_whitelisting",
-  "whitelisted_domains" : ["https://a573e282.ngrok.io"],
+  "whitelisted_domains" : ["https://cc2a797f.ngrok.io"],
   "domain_action_type": "add"
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAKziwfhwZC0BAH4J6aLp6jUVIN6aBsQu6fuhVNJ2h5hewA20y9ZA0cGh6oLKuymKT24a50HgO2wl2rQw9lqrKZBNSavN8gYHqZC73wot44O2QIiWZAqS9ePDWZBOfZCKEpR01jEIyTa3qn38Ns81KA8nZBRQlwukyIUqd3S0TFufQZDZD"
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAB8DxnfIC0BAM4vUNAq4bhadiG9NQPp5yIL3MkXZAFZAOdOP5y0JaKVlCAw8s4DeYAZAKpVP0GzAQKMUcIOQtEd4ZAGKiiLMoJKas1uS9TCYJRYMvzGd7WvY9NbxaOOIMy2tw2EXp5K3p1ZCfoglJxuWresnAUHlvOUbwBvtLwZDZD"
 
 
 # This initiates the home url for Kitty
 curl -X POST -H "Content-Type: application/json" -d ' {
   "home_url" : {
-     "url": "https://a573e282.ngrok.io/extension/welcome",
+     "url": "https://cc2a797f.ngrok.io/extension/welcome",
      "webview_height_ratio": "tall",
      "webview_share_button": "hide",
      "in_test":true
   }
 
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAKziwfhwZC0BALOQcGfsiinlFcoFzf7CQQoBd5C33MKNe4exC7bUvjpFZBzDtPuZCHacfJE5J9FUOqesmEsa1g2FAKAiJbg6Ttb3GVZBVCQwUc7fFh0zBc1rLld63Cjs7i7Sl48QHEeTu3k4XDEagVpj3HJkLdOO8KW21Wi1AZDZD"
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAB8DxnfIC0BAM4vUNAq4bhadiG9NQPp5yIL3MkXZAFZAOdOP5y0JaKVlCAw8s4DeYAZAKpVP0GzAQKMUcIOQtEd4ZAGKiiLMoJKas1uS9TCYJRYMvzGd7WvY9NbxaOOIMy2tw2EXp5K3p1ZCfoglJxuWresnAUHlvOUbwBvtLwZDZD"
 
 
 
 # This initiates the home url for a test of Kitty
 curl -X POST -H "Content-Type: application/json" -d ' {
   "home_url" : {
-     "url": "<YOUR NGROK>/extension/welcome",
+     "url": "https://cc2a797f.ngrok.io/extension/welcome",
      "webview_height_ratio": "tall",
      "webview_share_button": "hide",
      "in_test":true
   }
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=<YOUR TEST ACCESS TOKEN>"
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAB8DxnfIC0BAM4vUNAq4bhadiG9NQPp5yIL3MkXZAFZAOdOP5y0JaKVlCAw8s4DeYAZAKpVP0GzAQKMUcIOQtEd4ZAGKiiLMoJKas1uS9TCYJRYMvzGd7WvY9NbxaOOIMy2tw2EXp5K3p1ZCfoglJxuWresnAUHlvOUbwBvtLwZDZD"
 
 
 
