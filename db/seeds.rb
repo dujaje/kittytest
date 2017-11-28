4a4c678dae5ee78412773b35daf2fe1aaf3bc1f1
Alexander Haumer <alexander.haumer@me.com>
2017-11-28 08:58:35 +0000

    usershowpage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "Alex", last_name: "Benwa", user_id: 1)
+Group.create(group_id: 1, user_id: 1)
