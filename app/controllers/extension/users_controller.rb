9eb29fd3d154309d3eb3c8df83bf415ff16ad478
Jamie Dujardin <jamiedujardin@gmail.com>
2017-11-30 11:39:40 +0000

    completing user show

--- app/controllers/extension/users_controller.rb
+++ app/controllers/extension/users_controller.rb
@@ -1,5 +1,15 @@
 class Extension::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @users = User.all
+    @expenses_paid = Expense.where(user_id: @user.id)
+    @splits_owed = Split.where(user_id: @user.id)
+    @transactions = []
+    @expenses_paid.each do |expense|
+      @transactions << expense
+    end
+    @splits_owed.each do |split|
+      @transactions << split
+    end
   end
 end
