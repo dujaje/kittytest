e6f913cf9b6c9bb94a3e1fa6530353742d8ede29
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2017-11-30 12:58:26 +0000

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  routes.default_url_options[:host] = 'https://kittysplit.herokuapp.com/'
+  routes.default_url_options[:host] = 'https://kittysplit.herokuapp.com'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
